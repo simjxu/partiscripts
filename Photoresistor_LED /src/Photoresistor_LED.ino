49dac3bfe590d1b28bc5ab11bcd5042a38fed34d
Simon Xu <simonxu@Simons-MacBook-Pro.local>
2018-11-11 16:19:07 -0800

    added Google Sheets tester

--- Photoresistor_LED /src/Photoresistor_LED.ino
+++ Photoresistor_LED /src/Photoresistor_LED.ino
@@ -2,24 +2,35 @@
 // -----------------------------------------
 // Function and Variable with Photoresistors
 // -----------------------------------------
-// In this example, we're going to register a Particle.variable() with the cloud so that we can read brightness levels from the photoresistor.
-// We'll also register a Particle.function so that we can turn the LED on and off remotely.
+/*
+In this example, we're going to register a Particle.variable() with
+ the cloud so that we can read brightness levels from the photoresistor.
+We'll also register a Particle.function so that we can turn the LED on 
+and off remotely.
 
-// We're going to start by declaring which pins everything is plugged into.
-
-int led = D6; // This is where your LED is plugged in. The other side goes to a resistor connected to GND.
+We're going to start by declaring which pins everything is plugged into.
+*/
+int led = D6; // This is where your LED is plugged in. 
+							//The other side goes to a resistor connected to GND.
 int pwm = A4;
 
-int photoresistor = A0; // This is where your photoresistor is plugged in. The other side goes to the "power" pin (below).
+int boardled = D7;
 
-int power = A5; // This is the other end of your photoresistor. The other side is plugged into the "photoresistor" pin (above).
-// The reason we have plugged one side into an analog pin instead of to "power" is because we want a very steady voltage to be sent to the photoresistor.
-// That way, when we read the value from the other side of the photoresistor, we can accurately calculate a voltage drop.
+int photoresistor = A0; // This is where your photoresistor is plugged in. 
+												// The other side goes to the "power" pin (below).
 
-double analogvalue; // Here we are declaring the integer variable analogvalue, which we will use later to store the value of the photoresistor.
+int power = A5; // This is the other end of your photoresistor. The other side 
+// is plugged into the "photoresistor" pin (above).
+// The reason we have plugged one side into an analog pin instead of to 
+// "power" is because we want a very steady voltage to be sent to the photoresistor.
+// That way, when we read the value from the other side of the photoresistor,
+// we can accurately calculate a voltage drop.
+
+double analogvalue; // Here we are declaring the integer variable analogvalue, 
+										// which we will use later to store the value of the photoresistor.
 double setpoint;
 double minim = 40.0;
-double maxim = 340.0;
+double maxim = 300.0;
 double fadeamount = 1.0;
 double brightness = 0.0;
 
@@ -27,28 +38,43 @@ double brightness = 0.0;
 
 void setup() {
 
-    // First, declare all of our pins. This lets our device know which ones will be used for outputting voltage, and which ones will read incoming voltage.
-    pinMode(led,OUTPUT); // Our LED pin is output (lighting up the LED)
-    pinMode(photoresistor,INPUT);  // Our photoresistor pin is input (reading the photoresistor)
-    pinMode(power,OUTPUT); // The pin powering the photoresistor is output (sending out consistent power)
+	// First, declare all of our pins. This lets our device know which ones will be 
+	// used for outputting voltage, and which ones will read incoming voltage.
+	pinMode(led,OUTPUT); // Our LED pin is output (lighting up the LED)
+	pinMode(boardled,OUTPUT);		// This is the onboard led
+	pinMode(photoresistor,INPUT);  // Our photoresistor pin is input 
+																 // (reading the photoresistor)
+	pinMode(power,OUTPUT); // The pin powering the photoresistor is output 
+												 // (sending out consistent power)
 	pinMode(pwm, OUTPUT); // Pin for the pwm output
 
-    // Next, write one pin of the photoresistor to be the maximum possible, so that we can use this for power.
-    digitalWrite(power,HIGH);
+	// Next, write one pin of the photoresistor to be the maximum possible, so that 
+	// we can use this for power.
+	digitalWrite(power,HIGH);
 
-    // We are going to declare a Particle.variable() here so that we can access the value of the photoresistor from the cloud.
-    Particle.variable("analogvalue", &analogvalue, DOUBLE);
-    // This is saying that when we ask the cloud for "analogvalue", this will reference the variable analogvalue in this app, which is an integer variable.
+	// We are going to declare a Particle.variable() here so that we can access 
+	// the value of the photoresistor from the cloud.
+	Particle.variable("analogvalue", &analogvalue, DOUBLE);
+	// This is saying that when we ask the cloud for "analogvalue", this will reference 
+	// the variable analogvalue in this app, which is an integer variable.
 
-    // We are also going to declare a Particle.function so that we can turn the LED on and off from the cloud.
-    Particle.function("led",ledToggle);
-    // This is saying that when we ask the cloud for the function "led", it will employ the function ledToggle() from this app.
+	// Add brightness as variable to test it out
+	Particle.variable("brightness", &brightness, DOUBLE);
+	Particle.variable("setpoint", &setpoint, DOUBLE);
 
-    // bool success;
-    // success = Particle.publish("light_level");
-    // if (!success) {
-    // // get here if event publish did not work
-    // }
+
+	// We are also going to declare a Particle.function so that we can turn the LED on 
+	// and off from the cloud.
+	Particle.function("led",ledToggle);
+	// This is saying that when we ask the cloud for the function "led", it will employ 
+	//the function ledToggle() from this app.
+
+	// bool success;
+	// success = Particle.publish("light_level");
+	// if (!success) {
+	// // get here if event publish did not work
+	// }
+
 }
 
 
@@ -56,42 +82,58 @@ void setup() {
 
 void loop() {
 
-    // check to see what the value of the photoresistor is and store it in the int variable analogvalue
-    // To get the value of this variable, you will have to run
-    // curl https://api.particle.io/v1/devices/5c0032001951353338363036/analogvalue?access_token=39c7655424cc7f16ac8363b2c8913d3cccea141d
-    analogvalue = analogRead(photoresistor);
-    delay(5);
+	// check to see what the value of the photoresistor is and store it in the int variable analogvalue
+	// To get the value of this variable, you will have to run
+	// Ou need to change device ID, but access token is same for your account
+	// curl https://api.particle.io/v1/devices/5c0032001951353338363036/analogvalue?access_token=39c7655424cc7f16ac8363b2c8913d3cccea141d
+	analogvalue = analogRead(photoresistor);
+	delay(5);
 
-    // if (analogvalue < 2500) {
-    //     digitalWrite(led,HIGH);
-    // }
-    // else if (analogvalue > 2500) {
-    //     digitalWrite(led,LOW);
-    // }
-	
-    // min is 40, max is 340
-    if (analogvalue > minim && analogvalue < maxim) {
-        setpoint = 255-(analogvalue-minim)/300*255;
-    } else if (analogvalue <= minim) {
-		setpoint = 255;
+	// if (analogvalue < 2500) {
+	//     digitalWrite(led,HIGH);
+	// }
+	// else if (analogvalue > 2500) {
+	//     digitalWrite(led,LOW);
+	// }
+
+	// min is 40, max is 340
+	if (analogvalue > minim && analogvalue < maxim) {
+			setpoint = 255-(analogvalue-minim)/300*255;
+	} else if (analogvalue <= minim) {
+	setpoint = 254;
 	} else if (analogvalue >= maxim) {
 		setpoint = 0;
+	} else {
+		setpoint = 0;
 	}
-
 	pinMode(pwm, OUTPUT);
+
+	// // Set the D7 on board LED
+	// if (analogvalue > 100) {
+	// 	digitalWrite(boardled, LOW);
+	// } else {
+	// 	digitalWrite(boardled, HIGH);
+	// }
+
 	// Check the setpoint vs the brightness
-	if (brightness < setpoint && brightness < 253) {
+	if (brightness < setpoint && brightness < 252) {
 		brightness = brightness + fadeamount;
 		analogWrite(pwm, brightness);
 	} else if (brightness > setpoint && brightness > 1.3) {
 		brightness = brightness - fadeamount;
 		analogWrite(pwm, brightness);
+	} else {
+		brightness = brightness;
 	}
 	
-    // Publish light level event (change threshold to something lower if you want)
-    if (analogvalue < 200) {
-        Particle.publish("light_level");
-    }
+	// Publish light level event, which connects to slack webhook 
+	// (change threshold to something lower if you want)
+	if (analogvalue < 50) {
+	    Particle.publish("low_light", String(analogvalue), NO_ACK);
+	}
+
+	// Make sure it doesn't publish too much
+	delay(2000);
 }
 
 
@@ -99,16 +141,16 @@ void loop() {
 
 int ledToggle(String command) {
 
-    if (command=="on" || analogvalue < 2500) {
-        digitalWrite(led,HIGH);
-        return 1;
-    }
-    else if (command=="off" || analogvalue > 2500) {
-        digitalWrite(led,LOW);
-        return 0;
-    }
-    else {
-        return -1;
-    }
+	if (command=="on") {
+		digitalWrite(boardled,HIGH);
+		return 1;
+	}
+	else if (command=="off") {
+		digitalWrite(boardled,LOW);
+		return 0;
+	}
+	else {
+		return -1;
+	}
 
 }
---
+++ getpostwebhook-example/googlesheets_pgw.gs
@@ -0,0 +1,104 @@
+// Set Global Variables ------------------------------------------------------------------------------
+/* 
+ACCESS_TOKEN: Go to build.particle.io and click "Settings" on the left side panel
+PARTICLE_DEVICE_ID: Identify device ID from the console, or from using "particle list"
+SPREADSHEET_ID: Look on the URL for your Google Sheet for the long random string
+*/
+PropertiesService.getScriptProperties().setProperty('SHEET_NAME', 'Sheet1');
+PropertiesService.getScriptProperties().setProperty('PARTICLE_ACCESS_TOKEN', '<enter access token>');
+PropertiesService.getScriptProperties().setProperty('PARTICLE_DEVICE_ID', '<enter device id>');
+PropertiesService.getScriptProperties().setProperty('SPREADSHEET_ID', '<enter spreadsheetid>');
+
+// ---------------------------------------------------------------------------------------------------
+
+
+
+// Handle the webhook using doPost() -------------------------------------------------
+function doPost(e){
+  
+  // Set which spreadsheet to output this data to
+  var spreadsheet_id = PropertiesService.getScriptProperties().getProperty('SPREADSHEET_ID');
+  var sheet = SpreadsheetApp.openById(spreadsheet_id).getSheetByName( 
+    PropertiesService.getScriptProperties().getProperty('SHEET_NAME'));
+  
+  // Fill the "Webhook Data" cell with the parameters of the webhook package sent
+  sheet.getRange("D2").setValue(e.parameter);
+  
+  return ContentService.createTextOutput(JSON.stringify({text:"webhook received"})).setMimeType(ContentService.MimeType.JSON);;
+  
+}
+
+// Handle the button click for Post Request -------------------------------------------------
+function PostRequest() {
+  var spreadsheet_id = SpreadsheetApp.getActiveSpreadsheet().getId();
+  var sheet = SpreadsheetApp.openById(spreadsheet_id).getSheetByName( 
+    PropertiesService.getScriptProperties().getProperty('SHEET_NAME'));
+  
+  // API url goes like this:
+  // https://api.particle.io/v1/devices/54002d000751353530373132/
+  // led?args=on&access_token=39c7655424cc7f16ac8363b2c8913d3cccea141d
+  var device_id = PropertiesService.getScriptProperties().getProperty('PARTICLE_DEVICE_ID');
+  var access_token = PropertiesService.getScriptProperties().getProperty('PARTICLE_ACCESS_TOKEN');
+  var function_name = sheet.getRange("B4").getValue();
+  var request = sheet.getRange("B2").getValue();
+  var url = "https://api.particle.io/v1/devices/" + device_id + "/" + function_name + "?" + 
+    "access_token=" + access_token;
+
+  // Create a form which will be sent as the payload to the post request
+  var formData = {
+    'args' : request
+    
+  };
+  
+  // Create the options
+  var options =
+      {
+        "method"  : "POST", 
+        "payload"  : formData,
+        "followRedirects" : true,
+        "muteHttpExceptions": false
+      };
+  
+  // Send Post request body to the URL indicated
+  var result = UrlFetchApp.fetch(url, options);
+  
+  // Display response in Cell
+  if (result.getResponseCode() == 200) {  
+    var params = JSON.parse(result.getContentText());
+    sheet.getRange("B3").setValue(params);
+  }
+}
+
+// Handle the button click for Get Request -------------------------------------------------
+function GetRequest() {
+  // Start by trying in postman
+  
+  // Set which spreadsheet to pull or output information
+  var spreadsheet_id = SpreadsheetApp.getActiveSpreadsheet().getId();
+  var sheet = SpreadsheetApp.openById(spreadsheet_id).getSheetByName( 
+    PropertiesService.getScriptProperties().getProperty('SHEET_NAME'));
+  
+  // API url goes like this: 
+  // https://api.particle.io/v1/devices/<PARTICLE_DEVICE_ID>/<VARIABLE_NAME>?access_token=<PARTICLE_ACCESS_TOKEN>
+  var device_id = PropertiesService.getScriptProperties().getProperty('PARTICLE_DEVICE_ID');
+  var access_token = PropertiesService.getScriptProperties().getProperty('PARTICLE_ACCESS_TOKEN');
+  var variable_name = sheet.getRange("C2").getValue();
+  var url = "https://api.particle.io/v1/devices/" + device_id + "/" + variable_name + "?access_token=" + access_token
+  
+  // Not sure if all options are necessary
+  var options =
+      {
+        "method"  : "GET",   
+        "followRedirects" : true,
+        "muteHttpExceptions": true
+      };
+  
+  // Send Get request body to the URL indicated
+  var result = UrlFetchApp.fetch(url, options);
+  
+  // Display response in Cell
+  if (result.getResponseCode() == 200) {
+    var params = JSON.parse(result.getContentText());
+    sheet.getRange("C3").setValue(params);
+  };
+}
